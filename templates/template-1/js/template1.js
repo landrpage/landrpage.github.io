b45215bef341eeebc468dbe919bb284791131bbf
Landr Page <sunil@landr.page>
2020-05-23 22:01:16 +0530

    Changing website host to landr.page

--- templates/template-1/js/template1.js
+++ templates/template-1/js/template1.js
@@ -193,12 +193,11 @@ $("#live-preview").click(function() {
     
     if (website_session_uuid != "") {
         $("#spinnerLoaderOverlay").css("display", "none");
-        var preview_url = "http://localhost:9000/template-1/preview?uuid=" + website_session_uuid;
+        var preview_url = "https://landr.page/template-1/preview?uuid=" + website_session_uuid;
         window.open(preview_url, '_blank');
         return
     }
     var url = "https://landerbackend.sunilkumarc682.now.sh/website/session/details/";
-    // var url = "http://localhost:8000/website/session/details/";
     var websiteDetails = getWebsiteDetails();
     var body = JSON.stringify(websiteDetails);
     
@@ -216,7 +215,7 @@ $("#live-preview").click(function() {
             $("#spinnerLoaderOverlay").css("display", "none");
             website_session_uuid = response["website_details"]["uuid"];
             stripe_checkout_session_id = response["website_details"]["stripe_session_id"];
-            var preview_url = "http://localhost:9000/template-1/preview?uuid=" + website_session_uuid;
+            var preview_url = "https://landr.page/template-1/preview?uuid=" + website_session_uuid;
             window.open(preview_url, '_blank');
         },
         error: function(err) {
