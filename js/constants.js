b971d6f0bb2bcc75a3a887b37dfb85ffe229917c
Landr Page <sunil@landr.page>
2020-07-07 13:59:48 +0530

    Changing backend url

--- js/constants.js
+++ js/constants.js
@@ -2,4 +2,4 @@ var LANDR_FRONTEND_HOST = "https://landr.page";
 // var LANDR_BACKEND_HOST = "http://localhost:8000";
 
 // var LANDR_FRONTEND_HOST = "http://localhost:9000";
-var LANDR_BACKEND_HOST = "https://landerbackend.sunilkumarc682.now.sh";
+var LANDR_BACKEND_HOST = "https://lander-backend.vercel.app";
